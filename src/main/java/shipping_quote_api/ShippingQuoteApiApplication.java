ac1178e4f792ea4e21218009e30c3ab04074c660
pedrorodrigo <pedrorodrigo.mdasilva@gmail.com>
2026-08-26 15:41:30 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/shipping_quote_api/ShippingQuoteApiApplication.java
@@ -0,0 +1,13 @@
+package shipping_quote_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingQuoteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippingQuoteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shipping_quote_api/ShippingQuoteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package shipping_quote_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingQuoteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
