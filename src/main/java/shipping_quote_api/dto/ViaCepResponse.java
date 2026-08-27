31ced0a3a8a97e9cdd5230af00727c12ccb57121
pedrorodrigo <pedrorodrigo.mdasilva@gmail.com>
2026-08-27 15:34:22 -0300

    feat: add ViaCEP response DTO

---
+++ src/main/java/shipping_quote_api/dto/ViaCepResponse.java
@@ -0,0 +1,13 @@
+package shipping_quote_api.dto;
+
+public record ViaCepResponse(
+
+        String cep,
+        String logradouro,
+        String complemento,
+        String bairro,
+        String localidade,
+        String uf,
+        String estado
+
+) {}
