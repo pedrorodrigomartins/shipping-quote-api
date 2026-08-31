f5dce1a81702a79df96cd6b9a0f97bf884dc0734
pedrorodrigo <pedrorodrigo.mdasilva@gmail.com>
2026-08-31 15:11:26 -0300

    feat: integrate shipping providers into quote service

--- src/main/java/shipping_quote_api/service/ShippingService.java
+++ src/main/java/shipping_quote_api/service/ShippingService.java
@@ -2,19 +2,26 @@ package shipping_quote_api.service;
 
 import org.springframework.stereotype.Service;
 import shipping_quote_api.client.AddressClient;
+import shipping_quote_api.dto.ShippingOptionResponse;
 import shipping_quote_api.dto.ShippingQuoteRequest;
+import shipping_quote_api.dto.ShippingQuoteResponse;
 import shipping_quote_api.dto.ViaCepResponse;
+import shipping_quote_api.provider.ShippingProvider;
+
+import java.util.List;
 
 @Service
 public class ShippingService {
 
     private final AddressClient addressClient;
+    private final List<ShippingProvider> shippingProviders;
 
-    public ShippingService(AddressClient addressClient) {
+    public ShippingService(AddressClient addressClient, List<ShippingProvider> shippingProviders) {
         this.addressClient = addressClient;
+        this.shippingProviders = shippingProviders;
     }
 
-    public Void quote(ShippingQuoteRequest request) {
+    public ShippingQuoteResponse quote(ShippingQuoteRequest request) {
 
         ViaCepResponse origin =
                 addressClient.findAddressByZipCode(request.originZipCode());
@@ -22,6 +29,19 @@ public class ShippingService {
         ViaCepResponse destination =
                 addressClient.findAddressByZipCode(request.destinationZipCode());
 
+        List<ShippingOptionResponse> options = shippingProviders
+                .stream()
+                .map(provider -> provider.calculate(request))
+                .toList();
 
+        String originDescription = origin.localidade() + " - " + origin.uf();
+
+        String destinationDescription = destination.localidade() + " - " + destination.uf();
+
+        return new ShippingQuoteResponse(
+                originDescription,
+                destinationDescription,
+                options
+        );
     }
 }
